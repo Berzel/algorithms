13db41ae12827683bef21db4b467b517912e3216
Berzel <berzelbtumbude@gmail.com>
2019-11-20 16:51:35 +0200

    Added the linear search algorithm

---
+++ arrays/linear_search.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * Search for an item in an array using linear search
+ * 
+ * @param int *arr  The array that is being searched
+ * @param int size  The size of the array that is being searched
+ * @param int val The value we are searching for
+ **/
+int search(int *arr, int size, int val){
+  for (int i = 0; i < size; ++i){
+    if (arr[i] == val){
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+
+/**
+ * Run some tests
+ **/
+int main(int argc, char const *argv[]){
+  int A[] = {100,230,13,54,15,46,75,8,90,10}, n = 10;
+  int val = 13;
+
+  int found = search(A, n, val);
+
+  if (found > 0) {
+    printf("Value %d found at position %d.\n", val, found);
+  }
+
+  return 0;
+}
